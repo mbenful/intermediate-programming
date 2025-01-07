bcbb0a99de62b19820fb7c4d22fb0ca4e3a8c6e1
benfulm <michael_benful@horacemann.org>
2025-01-07 14:20:45 -0500

    extra credit update

--- NumberGuessGame/src/App.java
+++ NumberGuessGame/src/App.java
@@ -1,76 +1,16 @@
-import java.util.ArrayList;
-import java.util.Random;
-import java.util.Scanner;
-
 public class App {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        
-        int lowerNum = 0, upperNum = 0;
-        while (true) {
-            System.out.println("Enter the lower number of the range: ");
-            if (sc.hasNextInt()) {
-                lowerNum = sc.nextInt();
-                System.out.println("Enter the upper number of the range: ");
-                if (sc.hasNextInt()) {
-                    upperNum = sc.nextInt();
-                    if (lowerNum >= upperNum) {
-                        System.out
-                                .println("Invalid range! Lower number must be less than the upper number. Try again.");
-                    } else {
-                        break; 
-                    }
-                } else {
-                    System.out.println(sc.next() + " is not a valid number. Try again.");
-                }
-            } else {
-                System.out.println(sc.next() + " is not a valid number. Try again.");
-            }
+for(int i=1; i<4 ;i++){
+    int highscore = Integer.MAX_VALUE;
+    Game whatever = new Game(); 
+        whatever.playGame();
+        if (whatever.getScore() < highscore){
+            highscore = whatever.getScore();
+            System.out.println("new highscore is " +highscore);
         }
+        whatever.getScore();
+        System.out.println("final highscore is " + highscore);
+}
 
-       
-        Random random = new Random();
-        int number = random.nextInt(upperNum - lowerNum + 1) + lowerNum;
-
-        ArrayList<Integer> pastGuesses = new ArrayList<>();
-        System.out.println("I have picked a number between " + lowerNum + " and " + upperNum + ". Try to guess it!");
-
-        
-        while (true) {
-            System.out.println("Enter your guess: ");
-            if (sc.hasNextInt()) {
-                int guess = sc.nextInt();
-
-                if (pastGuesses.contains(guess)) {
-                    System.out.println("You already guessed " + guess + "! Try again.");
-                    continue;
-                }
-
-                pastGuesses.add(guess);
-
-                if (checkGuess(guess, number)) {
-                    System.out
-                            .println("Congrats! You guessed the number in " + pastGuesses.size() + " attempts.");
-                    break;
-                }
-            } else {
-                System.out.println(sc.next() + " is not a valid number, try again");
-            }
-        }
-
-        sc.close();
-    }
-
-    
-    static boolean checkGuess(int guess, int number) {
-        if (guess == number) {
-            return true;
-        } else if (guess > number) {
-            System.out.println("Lower");
-        } else {
-            System.out.println("Higher");
-        }
-        return false;
-    }
+    }   
 }
